b3ce3bc7d031291ad6c841814c984d16b36c7f03
Hema <hemamalini_2k@yahoo.com>
2014-11-19 20:55:00 -0800

    Adding GemFile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GestureCapture::Application.config.secret_token = 'ef69c692165fd709f5f086fb8ec1946cd169bd9398508ce9ad25b26e419339a78d944744b0dd223de2ee3107fc993d45ecc854a580dd08a4f3189f7317b3b816'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GestureCapture::Application.config.session_store :cookie_store, key: '_gesture_capture_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GestureCapture::Application.config.session_store :active_record_store
